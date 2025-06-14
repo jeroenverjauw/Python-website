f06aa90b1b34ffca4dc3d88382c71b6e45ec91fa
jeroenverjauw <jeroenverjauw@hotmail.com>
2025-06-14 18:06:40 +0000

    main.py update

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
-# import streamlit as st
-# from PIL import Image
-
-# st.header("Jeroen's first webpage", divider= 'gray')
-# st.subheader('Homepage')
-
-# value = st.text_input('some text here: ')
-# st.markdown('value: ' + value)
-# value2 = st.text_input('some text here2 : ')
-# st.markdown('value: ' + value2)
-# st.image("IMG_0748.JPG")
-
-# # st.sidebar.page_link("pages/Python widgets.py")
-
-# import streamlit as st
-# from st_pages import add_page_title, get_nav_from_toml
-
-# # st.set_page_config(layout="wide")
-
-# # If you want to use the no-sections version, this
-# # defaults to looking in .streamlit/pages.toml, so you can
-# # just call `get_nav_from_toml()`
-# nav = get_nav_from_toml("pages_sections.toml")
-
-# pg = st.navigation(nav)
-
-# add_page_title(pg)
-
-# pg.run()
-
 import streamlit as st
 
+Home = st.Page("pages/Home.py",title="")
 Circuit_generator = st.Page("pages/PythonWidgets/CircuitGenerator.py", title="Circuit generator", )
 writelatex = st.Page("pages/miscellaneous/WriteLaTeX.py", title="Write LaTeX", )
 
 pg = st.navigation(
         {
+            "": [Home,],
             "Python widgets": [Circuit_generator,],
             "Miscellaneous": [writelatex,]
         }
---
+++ pages/Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("Jeroen's first webpage", divider= 'gray')
+st.subheader('Homepage')
+st.write('Under construction')
